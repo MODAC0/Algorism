d3a244aed3732602ea081f93d7677ea585e3a0f0
MODAC0 <modac0302@gmail.com>
2023-01-05 13:39:21 +0900

    2023.01.03

---
+++ 28_treeBFS.js
@@ -0,0 +1,43 @@
+// todo: 문제: 임의의 tree를 구성하는 노드 중 하나의 Node 객체를 입력받아, 해당 노드를 시작으로 너비 우선 탐색(BFS, Breadth First Search)을 합니다. 이 때, 탐색되는 순서대로 노드의 값이 저장된 배열을 리턴해야 합니다.
+// ! 주의사항:
+
+let bfs = function (node) {
+  let queue = [node]; // node를 배열로 만든다
+  console.log(queue);
+  const values = [];
+  while (queue.length > 0) {
+    const head = queue[0];
+    queue = queue.slice(1);
+    values.push(head.value);
+    head.children.forEach((child) => queue.push(child));
+  }
+  return values;
+};
+
+// 이 아래 코드는 변경하지 않아도 됩니다. 자유롭게 참고하세요.
+let Node = function (value) {
+  this.value = value;
+  this.children = [];
+};
+
+// 위 Node 객체로 구성되는 트리는 매우 단순한 형태의 트리입니다.
+// membership check(중복 확인)를 따로 하지 않습니다.
+Node.prototype.addChild = function (child) {
+  this.children.push(child);
+  return child;
+};
+
+let root = new Node(1);
+root;
+let rootChild1 = root.addChild(new Node(2));
+rootChild1;
+let rootChild2 = root.addChild(new Node(3));
+let leaf1 = rootChild1.addChild(new Node(4));
+let leaf2 = rootChild1.addChild(new Node(5));
+let output = bfs(root);
+console.log(output); // --> [1, 2, 3, 4, 5]
+
+leaf1.addChild(new Node(6));
+rootChild2.addChild(new Node(7));
+output = bfs(root);
+console.log(output); // --> [1, 2, 3, 4, 5, 7, 6]
